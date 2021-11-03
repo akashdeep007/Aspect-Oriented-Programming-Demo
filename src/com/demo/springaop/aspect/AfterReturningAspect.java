d05e7c96bf4ad29cb28d7ef89537d157b4e8e3b4
akashdeep007 <beingakscool@gmail.com>
2021-11-03 11:46:07 +0530

    Post Processing Data in After Returning aspect

--- src/com/demo/springaop/aspect/AfterReturningAspect.java
+++ src/com/demo/springaop/aspect/AfterReturningAspect.java
@@ -1,9 +1,13 @@
 package com.demo.springaop.aspect;
 
+import java.util.List;
+
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+
+import com.demo.springaop.entity.Account;
 
 @Component
 @Aspect
@@ -12,9 +16,18 @@ public class AfterReturningAspect {
 	@Pointcut("execution(public * listAccount())")
 	private void p() {}
 	
-	@AfterReturning(pointcut = "p()")
-	public void afterReturning()
+	@AfterReturning(pointcut = "p()",returning = "result")
+	public void afterReturning(List<Account> result)
 	{
+		if(!(result.isEmpty()))
+		{
+			for(Account temp : result)
+			{
+				String username = temp.getUsername();
+				String upperCase = username.toUpperCase();
+				temp.setUsername(upperCase);
+			}
+		}
 		System.out.println("After Returning Aspect");
 	}
 }
